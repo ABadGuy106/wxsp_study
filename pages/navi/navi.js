e5f604151b201ea6fd63aed21804a95a04065d15
liujiwei <liujiwei0529@163.com>
2018-07-12 11:37:11 +0800

    页面跳转练习

--- pages/navi/navi.js
+++ pages/navi/navi.js
@@ -1,29 +1,20 @@
-var appinfo=getApp();
-
-// pages/test/test.js
+// pages/navi/navi.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    tdata:'测试数据',
-    dataForMe:null
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      dataForMe: appinfo.globalData.aTestData
-    })
+  
   },
-  toIndex: function(){
-    wx.navigateTo({
-      url: '../navi/navi',
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
